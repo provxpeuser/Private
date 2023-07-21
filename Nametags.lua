ca35dd16b882902898dc7908da6b25d2033e72c9
provxpeuser <112288490+provxpeuser@users.noreply.github.com>
2023-07-21 14:06:26 -0400

    Funny

--- Nametags.lua
+++ Nametags.lua
@@ -3,10 +3,8 @@ textChatService.OnIncomingMessage = function(message: TextChatMessage)
 	local properties = Instance.new("TextChatMessageProperties")
 	if message.TextSource then
 		local player = game:GetService("Players"):GetPlayerByUserId(message.TextSource.UserId)
-		if player.Name == player.Name == "RamainingsBadewarsss" then
-			properties.PrefixText = "<font color='#9A9AEB'>[VAPE PRIVATE (UWP)]</font> " .. message.PrefixText
-		if player.Name == player.Name == "BarbieMovieFan1" then
-			properties.PrefixText = "<font color='#9A9AEB'>[VAPE PRIVATE (MOBILE)]</font> " .. message.PrefixText
+		if player.Name == "menamge0" or player.Name == "RamainingsBadewarsss" then
+			properties.PrefixText = "<font color='#9A9AEB'>[VAPE PRIVATE (Wearish is ugly)]</font> " .. message.PrefixText
 		end
 	end
 	return properties
